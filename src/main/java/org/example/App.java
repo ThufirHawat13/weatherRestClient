425f428bb7cbd7775da945afb171fb2476108cbf
ThufirHawat13 <kurnyshov1998@gmail.com>
2022-11-25 21:56:01 +0300

    init

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,65 +8,63 @@ import java.util.*;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
-        final String sensorName = "TestSensor";
-        registerNewSensor(sensorName);
+public class App {
 
-        Random random = new Random();
+  public static void main(String[] args) {
+    final String sensorName = "TestSensor";
+    registerNewSensor(sensorName);
 
-        double minTemperature = -15d;
-        double maxTemperature = 15d;
-        double diff = maxTemperature - minTemperature;
+    Random random = new Random();
 
-        for(int i = 0; i <= 500; i++) {
-            double value = random.nextDouble() * diff + minTemperature;
-            sendNewMeasurement(Math.round(value * 10d)/10d, random.nextBoolean(), sensorName);
-        }
+    double minTemperature = -15d;
+    double maxTemperature = 15d;
+    double diff = maxTemperature - minTemperature;
 
-        }
+    for (int i = 0; i <= 500; i++) {
+      double value = random.nextDouble() * diff + minTemperature;
+      sendNewMeasurement(Math.round(value * 10d) / 10d, random.nextBoolean(), sensorName);
+    }
 
-        private static void registerNewSensor(String sensorName) {
-            final String url = "http://localhost:8080/sensors/register";
+  }
 
-            Map<String, Object> sensorJson = new HashMap<>();
-            sensorJson.put("name", sensorName);
+  private static void registerNewSensor(String sensorName) {
+    final String url = "http://localhost:8080/sensors/register";
 
-            makePostRequest(url, sensorJson);
-        }
+    Map<String, Object> sensorJson = new HashMap<>();
+    sensorJson.put("name", sensorName);
 
-        private static void sendNewMeasurement(double value, boolean raining, String sensorName) {
-            final String url = "http://localhost:8080/measurements/add";
+    makePostRequest(url, sensorJson);
+  }
 
-            Map<String, Object> measurementJson = new HashMap<>();
-            Map<String, Object> sensorJson = new HashMap<>();
-            measurementJson.put("value", value);
-            measurementJson.put("raining", raining);
-            sensorJson.put("name", sensorName);
-            measurementJson.put("sensor", sensorJson);
+  private static void sendNewMeasurement(double value, boolean raining, String sensorName) {
+    final String url = "http://localhost:8080/measurements/add";
 
+    Map<String, Object> measurementJson = new HashMap<>();
+    Map<String, Object> sensorJson = new HashMap<>();
+    measurementJson.put("value", value);
+    measurementJson.put("raining", raining);
+    sensorJson.put("name", sensorName);
+    measurementJson.put("sensor", sensorJson);
 
-            makePostRequest(url, measurementJson);
-        }
+    makePostRequest(url, measurementJson);
+  }
 
-        private static void  makePostRequest(String url, Map<String, Object> json) {
-        final RestTemplate restTemplate = new RestTemplate();
+  private static void makePostRequest(String url, Map<String, Object> json) {
+    final RestTemplate restTemplate = new RestTemplate();
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-        HttpEntity<Object> httpEntity = new HttpEntity<Object>(json, httpHeaders);
+    HttpEntity<Object> httpEntity = new HttpEntity<Object>(json, httpHeaders);
 
-        try {
-           String result = restTemplate.postForObject(url, json, String.class);
-        } catch (HttpClientErrorException e) {
-            System.out.println(e.getMessage());
-        }
+    try {
+      String result = restTemplate.postForObject(url, json, String.class);
+    } catch (HttpClientErrorException e) {
+      System.out.println(e.getMessage());
+    }
 
-        }
+  }
 
 
 }
